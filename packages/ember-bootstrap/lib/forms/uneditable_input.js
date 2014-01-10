afe1928af240c0346a28ede6ceac7c3cec5e3ffb
Tim Stirrat <tim.stirrat@gmail.com>
2014-01-10 15:34:58 -0800

    Fix duplicate 'classNames' key in uneditable input

--- packages/ember-bootstrap/lib/forms/uneditable_input.js
+++ packages/ember-bootstrap/lib/forms/uneditable_input.js
@@ -2,12 +2,11 @@ Bootstrap.Forms.UneditableInput = Bootstrap.Forms.Field.extend({
 
   inputField: Ember.View.extend({
     tagName:            'span',
-    classNames:         ['uneditable-input'],
+    classNames:         ['uneditable-input', 'form-control-static'],
     attributeBindings:  ['name'],
     template: Ember.Handlebars.compile('{{view.value}}'),
 
     valueBinding:       'parentView.value',
-    classNames:         ['form-control-static'],
     classNameBindings:  ['parentView.inputClassNames'],
     name: Ember.computed(function() {
       return this.get('parentView.name') || this.get('parentView.label');
